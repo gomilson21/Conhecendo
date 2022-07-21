1988c73ca6c328244da8bd66e83972b80ec2f1e5
gomilson21 <gomilson.otenta@gmail.com>
2022-07-21 21:36:20 +0100

    Visualizando o filtro do faturamento por loja e por quantidade de produtos vendidos

--- meuArquivo.py
+++ meuArquivo.py
@@ -6,13 +6,26 @@ fazendo assim a interação entre o projecto e uma base de dados em excel. '''
 
 # Importar a base de dados
 
-tabelas_vendas = pd.Vendas
+tabela_venda = pd.read_excel('Vendas.xlsx')
+
+''' Então para que o arquivo em excel seja lido é necessário instalar uma outra Biblioteca que faz
+leitura/abertura do arquivo python no arquivo xl. '''
 
 # Visualizar a base de dados
+pd.set_option('display.max_columns', None)
 
 # Faturamento por loja
+faturamento = tabela_venda[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+            # Comando usado para fazer filtragem dos campos na base de dados
+
+print(faturamento)
 
 # Quantidades de produtos vendidos por cada loja
+quantidade = tabela_venda[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+print("\n")
+print(quantidade)
+
 
 # ticket médio por produto em cada loja
 
