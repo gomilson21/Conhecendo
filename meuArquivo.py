64454a74a4176b1e74c2cafc504453e5964bf2de
gomilson21 <gomilson.otenta@gmail.com>
2022-07-21 18:59:13 +0100

    Instalação da Biblioteca pandas e importação da base de dados

---
+++ meuArquivo.py
@@ -0,0 +1,21 @@
+# Importar a Biblioteca Panda
+
+import pandas as pd
+''' Pandas é uma biblioteca usada associar um projecto ou arquivo python com o excel.
+fazendo assim a interação entre o projecto e uma base de dados em excel. '''
+
+# Importar a base de dados
+
+tabelas_vendas = pd.Vendas
+
+# Visualizar a base de dados
+
+# Faturamento por loja
+
+# Quantidades de produtos vendidos por cada loja
+
+# ticket médio por produto em cada loja
+
+# Enviar um email por relatório
+
+print("Ola, python and Panda")
